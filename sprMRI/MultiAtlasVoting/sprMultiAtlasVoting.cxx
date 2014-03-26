7258d993194cb1a862a8894c204e562ddba4c330
David Augusto Cárdenas Peña <dcardenasp@mint>
2014-03-26 17:55:16 -0500

    keep working on multiatlas

--- sprMRI/MultiAtlasVoting/sprMultiAtlasVoting.cxx
+++ sprMRI/MultiAtlasVoting/sprMultiAtlasVoting.cxx
@@ -1,174 +1,172 @@
 /*=========================================================================
  *
- *  Copyright Insight Software Consortium
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0.txt
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
  *
  *=========================================================================*/
 
+#include "vector"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
+#include "itkVectorImage.h"
 
-#include "vector"
+const    unsigned int   Dimension = 3;
+typedef  unsigned int   LabelSpelType;
 
-const    unsigned int    Dimension = 3;
-typedef  float           IntensitySpelType;
-typedef  float           LabelSpelType;
-const double PI = std::atan(1.0)*4;
 
 //  The types of the input images are instantiated by the following lines.
 //
-typedef itk::Image< LabelSpelType, Dimension >              LabelImageType;
+typedef itk::Image< LabelSpelType, Dimension >          LabelImageType;
+typedef itk::ImageFileReader< LabelImageType  >         LabelImageReaderType;
+typedef itk::ImageFileWriter< LabelImageType  >         LabelImageWriterType;
+typedef std::vector<LabelSpelType>                      VectorContainerType;
+typedef VectorContainerType::iterator                   VectorIteratorType;
+typedef itk::VectorImage<VectorContainerType,
+            Dimension>                                  VotingLabelType;
+typedef itk::ImageRegionConstIterator< LabelImageType > ConstLabelIteratorType;
+typedef itk::ImageRegionIterator< VotingLabelType >     VotingLabelIteratorType;
 
-typedef itk::ImageFileReader< LabelImageType  >             LabelImageReaderType;
-typedef itk::ImageFileWriter< LabelImageType  >             LabelImageWriterType;
-
-void CreateLabelImage(LabelImageType::Pointer p, LabelImageType::Pointer refImg)
+void CreateVotingImage(VotingLabelType::Pointer p, LabelImageType::Pointer refImg)
 {
   p->SetRegions(refImg->GetLargestPossibleRegion());
   p->SetOrigin(refImg->GetOrigin());
   p->SetSpacing(refImg->GetSpacing());
   p->SetDirection(refImg->GetDirection());
+  p->SetVectorLength(2);
   p->Allocate();
-  p->FillBuffer(static_cast<LabelImageType::PixelType>(0));
 }
-
 
 int main( int argc, char *argv[] )
 {
-  if( argc < 4 )
+  if( argc < 2 )
   {
     std::cerr << "Missing Parameters " << std::endl;
     std::cerr << "Usage:\n" << argv[0];
-    std::cerr << " numberOfLabels listOfSegmentations output_label_image";
+    std::cerr << " listOfImages";
     std::cerr << std::endl;
     return EXIT_FAILURE;
   }
 
-  unsigned int C = std::atoi(argv[1]);
+  /****Begin Variable declaration****/
+  unsigned int N = 0;                                                       //Number of images in the dataset
+  VotingLabelType::Pointer poll           = VotingLabelType::New();         //Image for storing votes
+  LabelImageReaderType::Pointer lblReader = LabelImageReaderType::New();
+  /****End Variable declaration****/
 
-  std::vector<LabelImageType::Pointer> images;
+  //Auxiliar variables:
+  LabelImageType::Pointer input;
+  std::ifstream file(argv[1]);
+  std::string lblName;
+  bool configAtlas = true;
 
+  while (getline(file, lblName, '\n'))
+  {
+    ++N;
+    lblReader->SetFileName( lblName );
+    try
+    {
+      lblReader->Update();
+    }
+    catch( itk::ExceptionObject & excp )
+    {
+      std::cerr << "Exception thrown while reading the image" << std::endl;
+      std::cerr << excp << std::endl;
+      return EXIT_FAILURE;
+    }
+    input = lblReader->GetOutput();
+    input->DisconnectPipeline();
+
+    if(configAtlas)
+    {
+      CreateVotingImage(poll, lblReader->GetOutput());
+      configAtlas = false;
+    }
+
+    //Setup Iterators:
+    ConstLabelIteratorType  itLbl(input,input->GetLargestPossibleRegion());
+    VotingLabelIteratorType itPoll(poll,poll->GetLargestPossibleRegion());
+
+    //MAIN LOOP:
+    for( itPoll.GoToBegin(), itLbl.GoToBegin(); !itLbl.IsAtEnd(); ++itPoll, ++itLbl )
+    {
+      LabelImageType::InternalPixelType c                 = itLbl.Get();
+      itk::VariableLengthVector<VectorContainerType> vote = itPoll.Get();
+
+      VectorIteratorType it = std::find(vote[0].begin(), vote[0].end(), c);
+
+      if(it==vote[0].end()) //If there's not that label at that spel
+      {
+        std::cout << vote.GetSize() << std::endl;
+        vote[0].push_back(c);//Include it...
+        vote[1];//.push_back(1);//with 1 vote
+        std::cout << "5\n";
+      }
+      else //just add 1 vote
+      {
+        std::cout << "6\n";
+        size_t index = std::distance(vote[0].begin(), it);
+        std::cout << "7\n";
+        vote[1].at(index) ++ ;//with 1 vote
+        std::cout << "8\n";
+      }
+    }
+  }
+
+
+
+/*
+
+  ImageType::IndexType start;
+  start.Fill(0);
+  ImageType::SizeType size;
+  size.Fill(2);
+  ImageType::RegionType region(start,size);
+  ImageType::Pointer image = ImageType::New();
+  image->SetRegions(region);
+  image->SetVectorLength(2);
+  image->Allocate();
+
+  ImageType::IndexType pixelIndex;
+  ImageType::PixelType pixelValue;
+  pixelIndex[0] = 1;
+  pixelIndex[1] = 1;
+  pixelIndex[2] = 1;
+  pixelValue = image->GetPixel(pixelIndex);
+  pixelValue[0].push_back(0);
+  pixelValue[0].push_back(2);
+  pixelValue[1].push_back(1);
+  pixelValue[1].push_back(3);
+  image->SetPixel(pixelIndex, pixelValue);
+
+  for ( int i = 0; i < pixelValue[0].size(); i++)
+  {
+    std::cout << pixelValue[0][i] << " " << pixelValue[1][i] << std::endl;
+  }
 
   /*
-  //Label template reader
-  LabelImageReaderType::Pointer lblReader   = IntensityImageReaderType::New();
-  lblReader->SetFileName(argv[2]);
+  ImageType::IndexType pixelIndex;
+  pixelIndex[0] = 1;
+  pixelIndex[1] = 1;
 
-  //Query image reader
-  IntensityImageReaderType::Pointer imgReader   = IntensityImageReaderType::New();
-  imgReader->SetFileName(argv[3]);
+  ImageType::PixelType pixelValue = image->GetPixel(pixelIndex);
+  std::cout << "pixel (1,1) = " << pixelValue << std::endl;
 
-  try
-  {
-    tptReader->Update();
-    lblReader->Update();
-    imgReader->Update();
-  }
-  catch( itk::ExceptionObject & excp )
-  {
-    std::cerr << "Exception thrown while reading the images" << std::endl;
-    std::cerr << excp << std::endl;
-    return EXIT_FAILURE;
-  }
+  typedef itk::VariableLengthVector<double> VariableVectorType;
+  VariableVectorType variableLengthVector;
+  variableLengthVector.SetSize(2);
+  variableLengthVector[0] = 1.1;
+  variableLengthVector[1] = 2.2;
 
-  //Intensity normalization
-  MRNormalizationFilterType::Pointer tptNormalizer = MRNormalizationFilterType::New();
-  tptNormalizer->SetInput( tptReader->GetOutput() );
-  tptNormalizer->Update();
+  image->SetPixel(pixelIndex, variableLengthVector);
 
-  MRNormalizationFilterType::Pointer imgNormalizer = MRNormalizationFilterType::New();
-  imgNormalizer->SetInput( imgReader->GetOutput() );
-  imgNormalizer->Update();
+  std::cout << "pixel (1,1) = " << pixelValue << std::endl;
 
 
-  // Create components required for registration
-  MetricType::Pointer                   metric          = MetricType::New();
-  TransformType::Pointer                transform       = TransformType::New();
-  OptimizerType::Pointer                optimizer       = OptimizerType::New();
-  IntensityInterpolatorType::Pointer    interpolator    = IntensityInterpolatorType::New();
-  RegistrationType::Pointer             registration    = RegistrationType::New();
+*/
 
-  //  Initialize the affine transform by using Image Intensity Moments
-  TransformInitializerType::Pointer     initializer     = TransformInitializerType::New();
-  initializer->SetTransform(   transform );
-  initializer->SetFixedImage(  imgNormalizer->GetOutput() );
-  initializer->SetMovingImage( tptNormalizer->GetOutput() );
-  initializer->MomentsOn();
-  initializer->InitializeTransform();
-  registration->SetInitialTransformParameters( transform->GetParameters() );
 
-  // Setup the Mattes mutual information metric
-  metric->SetNumberOfHistogramBins( 100 );
-  const unsigned int numberOfPixels  = imgNormalizer->GetOutput()->GetLargestPossibleRegion().GetNumberOfPixels();
-  const unsigned int numberOfSamples = static_cast< unsigned int >( numberOfPixels * 0.1 );
-  metric->SetNumberOfSpatialSamples( numberOfSamples );
 
-  // Each component is now connected to the instance of the registration method.
-  registration->SetMetric(        metric        );
-  registration->SetOptimizer(     optimizer     );
-  registration->SetTransform(     transform     );
-  registration->SetInterpolator(  interpolator  );
 
-  // Set the registration inputs
-  registration->SetFixedImage(       imgNormalizer->GetOutput() );
-  registration->SetMovingImage(      tptNormalizer->GetOutput() );
-  registration->SetFixedImageRegion( imgNormalizer->GetOutput()->GetLargestPossibleRegion() );
-
-  optimizer->SetMaximumStepLength( .1 ); // If this is set too high, you will get a
-  //"itk::ERROR: MeanSquaresImageToImageMetric(0xa27ce70): Too many samples map outside moving image buffer: 1818 / 10000" error
-
-  optimizer->SetMinimumStepLength( 0.01 );
-
-  // Set a stopping criterion
-  optimizer->SetNumberOfIterations( 200 );
-
-  try
-  {
-    registration->Update();
-  }
-  catch( itk::ExceptionObject & err )
-  {
-    std::cerr << "ExceptionObject caught !" << std::endl;
-    std::cerr << err << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  LabelInterpolatorType::Pointer    lblInterpolator = LabelInterpolatorType::New();
-  LabelResampleFilterType::Pointer  lblResampler    = LabelResampleFilterType::New();
-  lblResampler->SetInput(           lblReader->GetOutput() );
-  lblResampler->SetInterpolator(    lblInterpolator );
-  lblResampler->SetTransform(       registration->GetOutput()->Get() );
-  lblResampler->SetSize(            imgNormalizer->GetOutput()->GetLargestPossibleRegion().GetSize() );
-  lblResampler->SetOutputOrigin(    imgNormalizer->GetOutput()->GetOrigin() );
-  lblResampler->SetOutputSpacing(   imgNormalizer->GetOutput()->GetSpacing() );
-  lblResampler->SetOutputDirection( imgNormalizer->GetOutput()->GetDirection() );  
-  lblResampler->SetDefaultPixelValue( 0 );
-
-  LabelImageWriterType::Pointer lblWriter = LabelImageWriterType::New();
-  lblWriter->SetInput( lblResampler->GetOutput() );
-  lblWriter->SetFileName(argv[4]);
-
-  try
-  {
-    lblWriter->Update();
-  }
-  catch( itk::ExceptionObject & excp )
-  {
-    std::cerr << "Exception thrown while writing the image" << std::endl;
-    std::cerr << excp << std::endl;
-    return EXIT_FAILURE;
-  }*/
 
   return EXIT_SUCCESS;
 }
